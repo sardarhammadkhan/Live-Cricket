74cd06961d96f3f1911ae6953a2336080401c408
Hammad <hammad@ahmed.com>
2020-07-15 16:30:39 +0500

    my first commit

---
+++ js/fixture.js
@@ -0,0 +1,28 @@
+
+
+$(document).ready(function () {
+  $("#parent").on("click", function () {
+    $("#child").toggle();
+  });
+});
+
+$(document).ready(function () {
+  $("#parent2").on("click", function () {
+    $("#child2").toggle();
+  });
+});
+$(document).ready(function () {
+  $("#parent3").on("click", function () {
+    $("#child3").toggle();
+  });
+});
+$(document).ready(function () {
+  $("#parent4").on("click", function () {
+    $("#child4").toggle();
+  });
+});
+$(document).ready(function () {
+  $("#parent5").on("click", function () {
+    $("#child5").toggle();
+  });
+});
