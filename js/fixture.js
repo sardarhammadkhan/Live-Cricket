238b34c1f70f361c4eb83f2f9ba3ef43abf916f9
Hammad <hammad@ahmed.com>
2020-07-16 14:30:30 +0500

    second commit

--- js/fixture.js
+++ js/fixture.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function () {
   $("#parent").on("click", function () {
     $("#child").toggle();
@@ -19,6 +17,27 @@ $(document).ready(function () {
 $(document).ready(function () {
   $("#parent4").on("click", function () {
     $("#child4").toggle();
+  });
+});
+
+$(document).ready(function () {
+  $("#parent5").on("click", function () {
+    $("#child5").toggle();
+  });
+});
+$(document).ready(function () {
+  $("#parent6").on("click", function () {
+    $("#child6").toggle();
+  });
+});
+$(document).ready(function () {
+  $("#parent7").on("click", function () {
+    $("#child7").toggle();
+  });
+});
+$(document).ready(function () {
+  $("#parent8").on("click", function () {
+    $("#child8").toggle();
   });
 });
 $(document).ready(function () {
