7d188c32f392656d2a8c785f87061d3a183b6e89
Hammad <hammad@ahmed.com>
2020-07-22 16:28:28 +0500

    final

--- js/fixture.js
+++ js/fixture.js
@@ -25,11 +25,13 @@ $(document).ready(function () {
     $("#child5").toggle();
   });
 });
+
 $(document).ready(function () {
   $("#parent6").on("click", function () {
     $("#child6").toggle();
   });
 });
+
 $(document).ready(function () {
   $("#parent7").on("click", function () {
     $("#child7").toggle();
@@ -40,8 +42,33 @@ $(document).ready(function () {
     $("#child8").toggle();
   });
 });
+
 $(document).ready(function () {
-  $("#parent5").on("click", function () {
-    $("#child5").toggle();
+  $("#parent9").on("click", function () {
+    $("#child9").toggle();
+  });
+});
+
+$(document).ready(function () {
+  $("#parent10").on("click", function () {
+    $("#child10").toggle();
+  });
+});
+
+$(document).ready(function () {
+  $("#parent11").on("click", function () {
+    $("#child11").toggle();
+  });
+});
+
+$(document).ready(function () {
+  $("#parent12").on("click", function () {
+    $("#child12").toggle();
+  });
+});
+
+$(document).ready(function () {
+  $("#parent13").on("click", function () {
+    $("#child13").toggle();
   });
 });
